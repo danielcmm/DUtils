7e93ac5e7665b5052f1b1286feb4652ba5c0ae26
Daniel <danielcmm@gmail.com>
2016-12-14 16:02:17 -0200

    1.0.7 AddressUtils

---
+++ src/DUtils/AddressUtils.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DUtils;
+
+class AddressUtils{
+
+    public static function getStatesBR(){
+
+        return [
+            "AC" => "Acre",
+            "AL" => "Alagoas",
+            "AM" => "Amazonas",
+            "AP" => "Amapá",
+            "BA" => "Bahia",
+            "CE" => "Ceará",
+            "DF" => "Distrito Federal",
+            "ES" => "Espírito Santo",
+            "GO" => "Goiás",
+            "MA" => "Maranhão",
+            "MG" => "Minas Gerais",
+            "MS" => "Mato Grosso do Sul",
+            "MT" => "Mato Grosso",
+            "PA" => "Para",
+            "PB" => "Paraíba",
+            "PE" => "Pernambuco",
+            "PI" => "Piauí",
+            "PR" => "Paraná",
+            "RJ" => "Rio de Janeiro",
+            "RN" => "Rio Grande do Norte",
+            "RO" => "Rondônia",
+            "RR" => "Roraima",
+            "RS" => "Rio Grande do Sul",
+            "SC" => "Santa Catarina",
+            "SE" => "Sergipe",
+            "SP" => "São Paulo",
+            "TO" => "Tocantins"
+        ];
+
+    }
+
+}
